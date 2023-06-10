644050c21002acca904054c59731347717ba8230
Karina Lemos <karinalemos@id.uff.br>
2023-06-10 17:20:15 -0300

    Criação do scanner usando lex

---
+++ main.py
@@ -0,0 +1,16 @@
+from scannerrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr import Scanner
+
+# Gera o Scanner 
+#scanner = open('scanner.py','r')
+
+# Executa o scanner
+#exec(scanner.read())
+#print("Entrada processada com sucesso!")
+scanner = Scanner()
+print(scanner.executaScanner)
+yylex();
+
+
+#Arquivo de saída gerado pelo scanner 
+#saida = open('saida.txt', 'r')
+
